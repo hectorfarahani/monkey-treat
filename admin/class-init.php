e36fdec231b3a43e6c8e213971cf54d9c6dbaafe
Hector <6560172+hector-codes@users.noreply.github.com>
2021-11-15 00:19:18 +0330

    Add admin icon

--- admin/class-init.php
+++ admin/class-init.php
@@ -35,11 +35,14 @@ class Init {
 	public function add_menu_page() {
 		add_menu_page(
 			__( 'Banano Pay', 'banano-pay' ),
-			__( 'Menu page', 'banano-pay' ),
+			__( 'Banano Pay', 'banano-pay' ),
 			'manage_options',
 			'banano-pay',
 			array( $this, 'renbder_settings_page' ),
-			'dashicons-smiley',
+			'data:image/svg+xml;base64,' . base64_encode('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 35">
+			<path stroke="#000" stroke-linecap="round" stroke-width="3" d="M2 23v-1l1-1v-3l1-3 1-2 1-1 1-2 1-1V8l3-3 2-1h1l1-1h1l1-1h2a433 433 0 0 1 15 2l3 1h1l1 2 2 2 2 3 1 3v2l-1 1h-6l-4-1-1-1h-1l-1-1v-1h-1l-2-2h-5l-4 2-1 1-3 2-2 2-1 2-2 1-1 1-4 1H3v-1"/>
+			<path stroke="#000" stroke-linecap="round" stroke-width="3" d="m18 19 1 2 1 3 7 5 5 2 2 1h2l4 1h5l3-1 4-2 2-2 3-2 2-3 1-4 1-3 1-3-1-1c-1-2-3 0-5 0l-3 2-2 1-2 2-1 1-2 1-2 2v1h-2l-1 1-2 1h-2l-2-1-4-2-2-2-3-1-5-1-2 2"/>
+		  </svg>'),
 			28
 		);
 	}
